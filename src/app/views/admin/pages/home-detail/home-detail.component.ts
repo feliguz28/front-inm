e23ea2fe8b58d607aa2d2beb078282f8e5ac694f
feliguz26 <feliguz26@gmail.com>
2023-08-24 11:36:57 -0500

    entrega pre final

--- src/app/views/admin/pages/home-detail/home-detail.component.ts
+++ src/app/views/admin/pages/home-detail/home-detail.component.ts
@@ -21,7 +21,19 @@ export class HomeDetailComponent {
   Categories!: ArrayParametric;
   translateX = 0;
   public currentIndex = 0;
-  public imagesPerSection = 3;
+  public imagesPerSection = 4;
+  largeImageIndex: number | null = 0;
+  visibleImages: string[] = [];
+  startIndex = 0;
+
+  mockImageUrls: string[] = [
+    'https://www.bbva.com/wp-content/uploads/2021/04/casas-ecolo%CC%81gicas_apertura-hogar-sostenibilidad-certificado-.jpg',
+    'https://fincaraiz.com.co/blog/wp-content/uploads/2022/08/casas-modernas-1-1920x1130.jpg',
+    'https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FzYSUyMG1vZGVybmF8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+    'https://www.construyehogar.com/wp-content/uploads/2014/08/Dise%C3%B1o-de-casa-moderna-de-una-planta.jpg',
+    'https://definicion.de/wp-content/uploads/2011/01/casa-2.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp6S5ZyQUeeU0QEne2uCvkZC4NL5hK9adDaw&usqp=CAU'
+  ];
 
   constructor(
     private route: ActivatedRoute,
@@ -36,30 +48,31 @@ export class HomeDetailComponent {
     });
     this.getParametricData();
     this.getHomeById();
+    this.updateVisibleImages();
   }
 
   getHomeById() {
     this.homeService.getHomeById(this.idHomeEdit).subscribe(data => {
       this.home = data;
+      console.log(this.home);
     })
   }
 
   scrollGallery(direction: string): void {
-    const numImages = this.home.images.length;
-    
-    console.log('Current Index:', this.currentIndex);
-  
+    const numImages = this.mockImageUrls.length;
+
     if (direction === 'left') {
       this.currentIndex = (this.currentIndex - 1 + numImages) % numImages;
     } else if (direction === 'right') {
       this.currentIndex = (this.currentIndex + 1) % numImages;
     }
-  
-    console.log('Visible Indices:', this.getVisibleIndices());
+
+    this.updateVisibleImages();
+    this.largeImageIndex = this.currentIndex;
   }
 
   getVisibleIndices(): number[] {
-    const numImages = this.home.images.length;
+    const numImages = this.mockImageUrls.length;
     const visibleIndices = [];
 
     for (let i = 0; i < this.imagesPerSection; i++) {
@@ -68,6 +81,30 @@ export class HomeDetailComponent {
     }
 
     return visibleIndices;
+  }
+
+  showLargeImage(index: number): void {
+    this.largeImageIndex = index;
+  }
+
+  updateVisibleImages() {
+    this.visibleImages = [];
+
+    for (let i = 0; i < this.imagesPerSection; i++) {
+      const index = (this.startIndex + i) % this.mockImageUrls.length;
+      this.visibleImages.push(this.mockImageUrls[index]);
+    }
+  }
+
+  getCategoryLabel(): string {
+    if (this.home && this.home.categoryId) {
+      if (this.home.categoryId === 1) {
+        return 'Venta';
+      } else if (this.home.categoryId === 2) {
+        return 'Arriendo';
+      }
+    }
+    return 'Desconocido';
   }
 
   getParametricData() {
