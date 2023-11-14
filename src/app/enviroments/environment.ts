54852b6535bac1fe02de15b3b8d8816c1cf4462c
LUIS ALEJANDRO FERNANDEZ MEDINA <lafernandezm@ultragroupla.com>
2023-11-14 11:37:10 -0500

    publish final

--- src/app/enviroments/environment.ts
+++ src/app/enviroments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   //server:'https://apiinicio.azurewebsites.net',
   //server_api: 'https://www.bluesmartapi.somee.com/api',
-  server:'https://localhost:7235',
-  server_api: 'https://localhost:7235/api',
+  server:'http://193.203.182.127:8080',
+  server_api: 'http://193.203.182.127:8080',
 };
