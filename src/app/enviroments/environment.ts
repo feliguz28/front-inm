67b94638709c00cea67a61c0f5593866ed3e3991
LUIS ALEJANDRO FERNANDEZ MEDINA <lafernandezm@ultragroupla.com>
2023-11-14 11:56:24 -0500

    change api

--- src/app/enviroments/environment.ts
+++ src/app/enviroments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   //server:'https://apiinicio.azurewebsites.net',
   //server_api: 'https://www.bluesmartapi.somee.com/api',
-  server:'http://193.203.182.127:8080',
-  server_api: 'http://193.203.182.127:8080/api',
+  server:'https://www.bluesmartapi.somee.com',
+  server_api: 'https://www.bluesmartapi.somee.com/api',
 };
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -10,7 +10,7 @@ import { PagerRequestFilter } from '../models/pager-basic.interface';
 })
 export class HomeService {
 
-  url:string = "http://193.203.182.127:8080/api/"
+  url:string = "https://www.bluesmartapi.somee.com/api/"
 
   constructor(private http:HttpClient) {}
 
